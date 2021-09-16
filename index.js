3df0c9732fa7d6d16a6acbde630eba9007781ea8
staedtlernorica-R4 <longuong@live.com>
2021-09-16 14:06:29 -0400

    shows proper ending point

--- index.js
+++ index.js
@@ -4,68 +4,63 @@ $('input[type=submit]').on('click', function (event) {
 })
 
 
-function runQuiz(quizArray) {
+let entireQuizArray;
 
-  let currentQuestion = quizArray[0];
-  quizArray.shift();
-  displayQuestion(currentQuestion);
-}
+function displayQuestion(entireQuizArray) {
 
+  $('#question').text(entireQuizArray[0].question)
 
-
-function displayQuestion(currentQuestion) {
-
-  if (currentQuestion.type === 'boolean') {
-    $(".box2, .box3").hide();
-  } else {
-    $(".box2, .box3").show();
-  }
-
-  // console.log('Current Object:', currentQuestion);
-  // console.log('Current Question:', currentQuestion.question);
-  // console.log('Current Correct Answer:', currentQuestion.correct_answer);
-  // // console.log('Current Wrong Answer:',currentQuestion.incorrect_answer);
-  // console.log('Current Wrong Answers:', currentQuestion.incorrect_answers);
-
-  $('#question').text(currentQuestion.question)
-
-  const correctAnswer = currentQuestion.correct_answer;
-  let allAnswers = currentQuestion.incorrect_answers;
+  const correctAnswer = entireQuizArray[0].correct_answer;
+  let allAnswers = entireQuizArray[0].incorrect_answers;
   allAnswers.push(correctAnswer);
 
   // maybe dont shuffle Tue/False based on index because the order will always be False/True
   allAnswers.sort();
+
+  (entireQuizArray[0].type === 'boolean') ?
+    $(".box2, .box3").hide() :
+    $(".box2, .box3").show(), allAnswers.reverse();
+
 
   // need to remove #correct after each question
   const correctAnswerIndex = allAnswers.indexOf(correctAnswer);
   $(`.box${correctAnswerIndex}`).attr('id', 'correct');
 
 
-
   for (i = 0; i < allAnswers.length; i++) {
-    $(`.box${i}`).text(allAnswers[i])
+    $(`.box${i}`).text(allAnswers[i]);
   }
 }
 
 
-
-
 function checkAnswer(boxObject) {
+
+  entireQuizArray.shift();
+
   if (boxObject[0].id === 'correct') {
-    // alert('correct!');
     boxObject[0].id = '';
     boxObject.addClass('right');
+
   } else {
-    // alert('wrang!!');
     boxObject.addClass('wrong');
     $('#correct').addClass('right');
     $('.box').attr('id', '');
-    // debugger
   }
 
-  setTimeout(function () { 
-    $('.box').removeClass('right wrong')
-    getQuizArray() }, 1500);
+
+  setTimeout(function () {
+
+    // if entireQuizArray is empty, exits loop
+
+    if (entireQuizArray.length === 0) {
+      alert('no more questions left');
+      return null
+    }
+
+    $('.box').removeClass('right wrong');
+    displayQuestion(entireQuizArray);
+  },
+    1500)
 }
 
 
@@ -74,23 +69,24 @@ $('.box').on('click', function () {
 });
 
 
+
 // run only once; pass data to runQuiz();
-function getQuizArray(number = 10, category, difficulty, type) {
+function getQuizArrayAndStart(number = 4, category = '', difficulty = '', type = 'boolean') {
 
   $.ajax({
     url: "https://opentdb.com/api.php",
     method: "GET",
     dataType: "json",
     data: {
-      amount: '2',       //any number
-      category: '',     //9 to 32
-      difficulty: '',     //easy, medium or hard
-      type: '',            //multiple or boolean
+      amount: number,       //any number
+      category: category,     //9 to 32
+      difficulty: difficulty,     //easy, medium or hard
+      type: type,            //multiple or boolean
       encode: ''      //base64, url3986, '' = default encoding
 
     }
   }).then(function (data) {
-    runQuiz(data.results);  //pass quiz data onto askQuestion()
+    entireQuizArray = data.results;
+    displayQuestion(entireQuizArray);  //pass quiz data onto askQuestion()
   });
-
 }
